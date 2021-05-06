193ffdaf1ac5f4b53805bcfa5dd8c0a8febba368
gar212 <gartoyou@gmail.com>
2021-05-07 03:10:57 +1200

    add variable file for education cards

---
+++ src/components/MainSection/EducationInfo.js
@@ -0,0 +1,41 @@
+import MissionReady from '../../img/mr.jpg';
+import Aut from '../../img/aut.jpg';
+import HowickCollege from '../../img/howick.jpg';
+
+// Object array for Education Cards
+const EducationInfo = [
+    {
+        name: "Mission Ready",
+        image: MissionReady,
+        subText: "Full Stack Development",
+        keyPoints: [
+            "Currently studying (Jan 2021 - Now)",
+            "Create responsive web apps",
+            "Backend & Database development",
+            "Coding best practices"
+        ]
+    },
+    {
+        name: "Auckland University of Technology",
+        image: Aut,
+        subText: "Bachelor of Digital Design",
+        keyPoints: [
+            "Graduated in 2016",
+            "2D & 3D Animation",
+            "Character Rigging",
+            "Film & Game Design"
+        ]
+    },
+    {
+        name: "Howick College",
+        image: HowickCollege,
+        subText: "NCEA Level 3",
+        keyPoints: [
+            "Graduated in 2014",
+            "Graphic Design",
+            "Information Technology"
+        ]
+    },
+]
+
+export default EducationInfo
